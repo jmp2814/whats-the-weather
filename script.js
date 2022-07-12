4b077d5046b1075368347d4ccf77948d4f3d416e
jmp2814 <jmp2814@gmail.com>
2022-07-12 10:53:43 -0400

    searched cities show up as buttons

--- script.js
+++ script.js
@@ -1,14 +1,70 @@
 var submitEl = document.querySelector("#submitBtn");
 var inputEl = document.querySelector("#cityInput");
 var msgEl = document.querySelector(".msg");
-var APIkey = "a624cbc58502929687c870d48d2bc712";
+var cityList = document.querySelector("#prev-city");
 var city;
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}"
+const APIkey = "a624cbc58502929687c870d48d2bc712";
+const baseAPIurl = "https://api.openweathermap.org";
+
+function fetchLocation(searchText) {
+    const apiUrl = `${baseAPIurl}/geo/1.0/direct?q=${searchText}&limit=5&appid=${APIkey}`;
+    fetch(apiUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log("Data is: ", data);
+            if (!data[0]) {
+                alert("Location not found!");
+                return;
+            }
+            fetchWeather(data[0].lat, data[0].lon)
+        })
+}
+
+function fetchWeather(latitude, longitude) {
+
+    var queryURL = `${baseAPIurl}/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${APIkey}`;
+
+    fetch(queryURL)
+        .then(function (resonse) {
+            return resonse.json();
+        })
+        .then(function (data) {
+            // var supText = "o";
+            console.log(data);
+            document.getElementById("temp-span").innerText=data.current.temp;
+            document.getElementById("wind-span").innerText=data.current.wind_speed + " MPH";
+            document.getElementById("humi-span").innerText=data.current.humidity;
+            document.getElementById("uv-span").innerText=data.current.uvi;
+        
+        })
+
+}
 
 function citySubmit(event) {
+
     event.preventDefault();
     console.log(event);
     console.log(inputEl.value);
+    fetchLocation(inputEl.value);
+    cityAdd();
+    // var ul = cityList;
+    // var li = document.createElement("button");
+    // li.innerHTML = inputEl.value;
+    // ul.appendChild(li);
+
+
+}
+
+// function to create button for previously searched cities and add it to a list below search bar
+
+function cityAdd() {
+    var cityUl = cityList
+    var cityLi = document.createElement("button");
+    cityLi.innerHTML = inputEl.value;
+    cityUl.appendChild(cityLi)
+    cityLi.classList.add("prevCityBtn");
 }
 
 submitEl.addEventListener("click", citySubmit);
